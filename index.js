02f038cb2f8a3dc21971c4e10c17a4044eaf2c09
Noble Obioma <nobioma1@gmail.com>
2019-07-08 13:32:46 +0100

    Create route to update a user

--- index.js
+++ index.js
@@ -81,8 +81,35 @@ server.delete(`${routes.api}/:id`, async (req, res) => {
   }
 });
 
+server.put(`${routes.api}/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await db.findById(id);
 
-server.delete(routes.api, (req, res) => {});
+    if (user) {
+      const name = req.body.name.trim();
+      const bio = req.body.bio.trim();
+
+      if (name && bio) {
+        const newUserDetails = { name, bio };
+        const newUserId =  await db.update(id, newUserDetails);
+        res.status(200).json({ ...newUserId, ...newUserDetails });
+      } else {
+        res
+          .status(400)
+          .json({ message: 'Please provide name and bio for the user.' });
+      }
+    } else {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    }
+  } catch {
+    res.status(500).json({
+      error: 'There was an error while saving the user to the database',
+    });
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`Server is live @ 127.0.0.1:${PORT}`);
