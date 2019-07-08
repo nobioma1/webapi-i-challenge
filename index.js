5564c09492525d74a094b683276e3182d7c01d70
Noble Obioma <nobioma1@gmail.com>
2019-07-08 13:32:04 +0100

    Create route to get user by ID

--- index.js
+++ index.js
@@ -45,7 +45,25 @@ server.get(routes.api, async (req, res) => {
   }
 });
 
-server.get(routes.api, (req, res) => {});
+server.get(`${routes.api}/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await db.findById(id);
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The user with the specified ID does not exist.' });
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ error: 'The user information could not be retrieved.' });
+  }
+});
+
 
 server.delete(routes.api, (req, res) => {});
 
