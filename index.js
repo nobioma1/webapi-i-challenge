656c1ebb4da69f06e20923cb3405d2fca0ebd5df
Noble Obioma <nobioma1@gmail.com>
2019-07-08 12:31:26 +0100

    Initial commit, Setup server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+server.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// routes
+server.get('/', (req, res) => {
+  res.status(200).send('Server Up')
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is live @ 127.0.0.1:${PORT}`);
+});
